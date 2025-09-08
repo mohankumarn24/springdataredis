98058aa084c925c8b76024e2257bb1074e36e0c4
Mohan Kumar N <mohankumar.n24@outlook.com>
2025-09-08 22:23:58 +0530

    refactor

--- src/main/java/net/projectsync/redis/SpringDataRedisExampleApplication.java
+++ src/main/java/net/projectsync/redis/SpringDataRedisExampleApplication.java
@@ -12,3 +12,10 @@ public class SpringDataRedisExampleApplication {
         SpringApplication.run(SpringDataRedisExampleApplication.class, args);
     }
 }
+
+/* STEPS:
+- Double click on ./Redis-x64-3.0.504/redis-server.exe to start Redis
+    -- https://github.com/microsoftarchive/redis/releases
+    -- https://www.youtube.com/watch?v=4_bwialzt1M
+- View data in 'Another Redis Desktop Manager' client
+ */
